918ff9b4edde4213a9001d9d837a5a7f81a9b837
Ahmad Sheikhy <asheikhy201@gmail.com>
2024-12-25 11:41:42 +0330

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace RecursiveFibonacci
+{
+    class Program
+    {
+        static int fib(int x)
+        {
+            if (x < 3)
+                return x-1;
+            return fib(x - 1) + fib(x - 2);
+        }
+        static void Main(string[] args)
+        {
+            int userNum;
+            string number;
+            number = Console.ReadLine();
+            userNum = int.Parse(number);
+            Console.WriteLine(fib(userNum));
+            Console.ReadKey();
+        }
+    }
+}
